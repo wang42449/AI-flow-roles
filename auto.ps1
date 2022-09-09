e53c1ccc554cea270de5c1f4de54da2549dd0320
wang42449 <63088118+wang42449@users.noreply.github.com>
2022-09-09 11:12:23 +0800

    Delete auto.ps1

--- auto.ps1
+++
@@ -1,9 +0,0 @@
-mkdir C:\Users\webdav
-Invoke-WebRequest -Uri https://app.raidrive.com/d86ea6fa40f74010914976063f94774b/setup/RaiDriveSetup.exe -OutFile C:\Users\webdav\raidrive.exe
-Start-Process C:\Users\webdav\raidrive.exe /S -NoNewWindow -Wait -PassThru 
-Invoke-WebRequest -Uri https://github.com/gylang/aliyundriver-webdav-software-ui/archive/refs/heads/master.zip -OutFile C:\Users\webdav\aliyun.zip
-Invoke-WebRequest -Uri https://github.com/messense/aliyundrive-webdav/releases/download/v1.10.1/aliyundrive-webdav-v1.10.1.x86_64-pc-windows-msvc.zip -OutFile C:\Users\webdav\aliwebdav.zip
-Expand-Archive -Path "C:\Users\webdav\aliyun.zip" -DestinationPath "C:\Users\webdav" -Confirm
-Expand-Archive -Path "C:\Users\webdav\aliwebdav.zip" -DestinationPath "C:\Users\webdav" -Confirm
-Start-Process C:\Users\webdav\aliyundriver-webdav.exe
-Start-Process C:\Program Files\OpenBoxLab\RaiDrive\RaiDrive.exe
