806efeeaa5d61189efa98dc86e50789503bc1ec4
wang42449 <63088118+wang42449@users.noreply.github.com>
2022-09-09 11:13:39 +0800

    Create ali.ps1

---
+++ aliyundriver/ali.ps1
@@ -0,0 +1,11 @@
+mkdir C:\Users\webdav
+Invoke-WebRequest -Uri https://downloads.rclone.org/v1.59.1/rclone-v1.59.1-windows-amd64.zip -OutFile C:\Users\webdav\rclone.zip
+Expand-Archive -Path "C:\Users\webdav\rclone.zip" -DestinationPath "C:\Users\webdav" -Confirm
+Invoke-WebRequest -Uri https://github.com/gylang/aliyundriver-webdav-software-ui/archive/refs/heads/master.zip -OutFile C:\Users\webdav\aliyun.zip
+Invoke-WebRequest -Uri https://github.com/messense/aliyundrive-webdav/releases/download/v1.10.1/aliyundrive-webdav-v1.10.1.x86_64-pc-windows-msvc.zip -OutFile C:\Users\webdav\aliwebdav.zip
+Expand-Archive -Path "C:\Users\webdav\aliyun.zip" -DestinationPath "C:\Users\webdav" -Confirm
+Expand-Archive -Path "C:\Users\webdav\aliwebdav.zip" -DestinationPath "C:\Users\webdav" -Confirm
+Move-Item C:\Users\webdav\aliyundriver-webdav.exe C:\Users\webdav\aliyundriver-webdav-software-ui-master\bin\aliyundriver-webdav.exe
+Start-Process C:\Users\webdav\aliyundriver-webdav-software-ui-master\aliyundriver-webdav.exe
+Start-Process -FilePath https://www.aliyundrive.com/
+Start-Process C:\Users\webdav\rclone-v1.59.1-windows-amd64\rclone.exe config && cmd /c "C:\Users\webdav\rclone-v1.59.1-windows-amd64\rclone.exe mount webdav:/ Z: --vfs-cache-mode writes"
