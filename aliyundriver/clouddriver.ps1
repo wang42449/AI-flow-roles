c984db3bff2d61d82066cfcabae099fcd45d74bc
wang42449 <63088118+wang42449@users.noreply.github.com>
2022-09-12 08:52:28 +0800

    Update clouddriver.ps1

--- aliyundriver/clouddriver.ps1
+++ aliyundriver/clouddriver.ps1
@@ -1,5 +1,7 @@
-Invoke-WebRequest -Uri http://yd.downxia.com/down/zlybnbdyp.rar -OutFile C:\Users\webdav\clouddrive.rar
-Install-Module -Name 7Zip4Powershell
-Expand-7Zip -ArchiveFileName C:\Users\webdav\cloudrive.rar -TargetPath C:\Users\webdav
+mkdir C:\Users\webdav
+Invoke-WebRequest -Uri https://www.nite07.com/file/c/CloudDriveSetup-X64-1.1.63.exe -OutFile C:\Users\webdav\clouddrive.exe
 Start-Process C:\Users\webdav\zlybnbdyp\CloudDrive_1.1.63_64bit_Setup.exe
 Start-Process -FilePath https://www.aliyundrive.com/
+Start-Process -FilePath https://www.wolai.com/8zZ7RGnMUWsvBm9SWfZ6dz
+Start-Process -FilePath https://blog.csdn.net/qq_34580627/article/details/125262931
+explorer
